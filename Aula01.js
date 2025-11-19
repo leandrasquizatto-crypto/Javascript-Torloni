c3544f2e83ff0045add4c7571d30fcf53a2fe143
LEANDRA FONTES SQUIZATTO <56184686895@imaccommand034.local>
2025-11-19 11:24:06 -0300

    exercicio de js

---
+++ Aula01.js
@@ -0,0 +1,10 @@
+window.alert("minha primeira mensagem")
+
+
+window.confirm("Aula 01.js")
+
+let nome = window.prompt("Qual è seu nome ?")
+
+window.alert("Bem vindo(a)!" + nome )
+
+console.log("Apresentando mensagem na tela" )
