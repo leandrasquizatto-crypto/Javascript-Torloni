46422d88c7d1d960f8bf750fb59653a0dd7a2d50
LEANDRA FONTES SQUIZATTO <56184686895@imaccommand034.local>
2025-11-19 16:26:58 -0300

    Exercicio finalizado

--- Exercicio04.js
+++ Exercicio04.js
@@ -1,22 +1,43 @@
-let n1 = parseInt(prompt("Digite sua primeira nota "))
+// let n1 = parseInt(prompt("Digite sua primeira nota "))
 
-let n2 = parseInt(prompt("Digite sua segunda nota "))
+// let n2 = parseInt(prompt("Digite sua segunda nota "))
 
-let n3 = parseInt(prompt("Digite sua terceira nota "))
+// let n3 = parseInt(prompt("Digite sua terceira nota "))
 
-let n4 = parseInt(prompt("Digite sua quarta nota "))
+// let n4 = parseInt(prompt("Digite sua quarta nota "))
 
-// Calculando a média
+// // Calculando a média
 
-let soma = (n1 + n2 + n3 + n4) / 4;
+// let soma = (n1 + n2 + n3 + n4) / 4;
 
-console.log(soma)
+// console.log(soma)
 
-if (soma >= 7.0) {
-    console.log("APROVADO");
+// if (soma >= 7.0) {
+//     console.log("APROVADO");
 
-} else if (soma >= 5.0) {
-    console.log("RECUPERAÇÃO");
-} else {
-    console.log("REPROVADO");
+// } else if (soma >= 5.0) {
+//     console.log("RECUPERAÇÃO");
+// } else {
+//     console.log("REPROVADO");
+// } 
+
+
+alert("Informe ás notas de 0 á 10 ")
+let nota1 = parseFloat(prompt("Digite a primeira nota : "))
+let nota2 = parseFloat(prompt("Digite a segunda nota : "))
+let nota3 = parseFloat(prompt("Digite a terceira nota : "))
+let nota4 = parseFloat(prompt("Digite a quarta nota : "))
+
+let media = (nota1 + nota2 + nota3 + nota4)/4
+
+console.log ("A média do aluno é: " + média)
+
+if(media >= 7){
+    console.log ( "Aprovado")
+}
+else if ( media < 7 && media>= 5){
+    console.log ( "Recuperação")
+} 
+else{ 
+    console.log( "Reprovado ;-;")
 }
---
+++ Exercicio05.js
@@ -0,0 +1,15 @@
+ //Escreva um programa que solicita três números ao usuário e verifica se eles estão em ordem
+//crescente. Exibindo uma mensagem indicando se os números estão em ordem crescente ou
+//não.
+let nota1 = parseFloat(prompt("Digite a primeira nota : "))
+let nota2 = parseFloat(prompt("Digite a segunda nota : "))
+let nota3 = parseFloat(prompt("Digite a terceira nota : "))
+let nota4 = parseFloat(prompt("Digite a quarta nota : "))
+
+if (n1 < n2 &&  n3 > n4 ) {
+    console.log(" Está em ordem crescente")
+} 
+
+else  {
+    console.log(" Não está em ordem crescente")
+}
